bfb875c85cd9e87b1c7a946ec24a1d0ff78392d7
Duber1234 <dfpesca02@misena.edu.co>
2015-08-26 13:59:05 -0500

    fallo de la clase usuario

--- Vista/login.php
+++ Vista/login.php
@@ -12,13 +12,25 @@ and open the template in the editor.
     <body>
         <div>
             <form action="" method="" id="login">
-                <input type="text" name="username" id="username" placeholder="Username" />
-                <input type="password" name="password" id="password" placeholder="Password" />
+                <input type="text" name="username" id="username" placeholder="Username" /><br>
+                <input type="password" name="password" id="password" placeholder="Password" /><br><br>
                 <input type="submit" name="iniciar" id="iniciar" value="Iniciar Sesion"/>
                 
             </form>
             
-            
+            <p>
+                <?php
+              include '../Modelo/clUsuario.php'; 
+              
+              $usuario = clUsuario::buscar("SELECT * FROM Usuarios where username=? and password=?", array("duber","123"));
+              
+              echo $usuario->getIdUsuario();
+              
+              
+                ?>
+                
+                
+            </p>
         </div>
     </body>
 </html>
