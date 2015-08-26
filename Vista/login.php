0c393d3bd50bf78c62c5582f4ae0f7c54917e98a
Duber1234 <dfpesca02@misena.edu.co>
2015-08-26 13:21:01 -0500

    vista del login

---
+++ Vista/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div>
+            <form action="" method="" id="login">
+                <input type="text" name="username" id="username" placeholder="Username" />
+                <input type="password" name="password" id="password" placeholder="Password" />
+                <input type="submit" name="iniciar" id="iniciar" value="Iniciar Sesion"/>
+                
+            </form>
+            
+            
+        </div>
+    </body>
+</html>
